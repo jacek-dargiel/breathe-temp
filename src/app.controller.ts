2294b8714e3bcd4a96fe3ed9876b159dd5e05768
Jacek Dargiel <jacek.dargiel@10clouds.com>
2019-06-28 08:55:32 +0200

    Average values over time

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { singleton } from 'tsyringe';
 import { DeviceService } from './device.service';
-import { throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { throwError, Observable } from 'rxjs';
+import { catchError, bufferTime, map, filter } from 'rxjs/operators';
 import { retryBackoff } from 'backoff-rxjs';
 import { LoggerService } from './logger.service';
 
@@ -19,11 +19,31 @@ export class AppController {
           error.device && error.device.destroy();
           return throwError(error)
         }),
-        retryBackoff(10000)
+        retryBackoff(10000),
+        this.averageOverTime(120000),
+        map(value => this.toFixed(value, 2)),
       )
       .subscribe(temperature => {
         this.logger.log(temperature);
       });
+  }
+
+  private averageOverTime(time: number) {
+    return (source$: Observable<number>) => {
+      return source$.pipe(
+        bufferTime(time),
+        filter(values => values.length > 0),
+        map(values => this.sum(values)/values.length)
+      )
+    }
+  }
+
+  private sum(values: number[]) {
+    return values.reduce((sum, value) => sum + value, 0);
+  }
+
+  private toFixed(value: number, digits: number): number {
+    return parseFloat(value.toFixed(digits));
   }
 
 }
